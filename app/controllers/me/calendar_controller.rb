000cfabc19aa2b43a03c980d437a917182c2cdc1
cosch <github@torfbruecke.net>
2012-08-27 16:23:53 +0200

    cleanup calendar controler

--- app/controllers/me/calendar_controller.rb
+++ app/controllers/me/calendar_controller.rb
@@ -1,6 +1,7 @@
 class Me::CalendarController < Me::BaseController
 
   helper :action_bar
+
   def search
     if request.post?
       path = parse_filter_path(params[:search])
@@ -11,51 +12,9 @@ class Me::CalendarController < Me::BaseController
   end
 
   def index
-    @month = (params[:month] || (Time.zone || Time).now.month).to_i
-    @year = (params[:year] || (Time.zone || Time).now.year).to_i
-
-    @shown_month = Date.civil(@year, @month)
-
-    @event_strips = Event.event_strips_for_month(@shown_month)
+    list
   end
 
-
-  def list_orig
-    logger.debug "CalendarController list in"
-    @path.default_sort('updated_at')
-    full_url = url_for(:controller => '/me/pages/calendar', :action => 'list', :path => @path)
-
-    @pages = Page.paginate_by_path(@path.merge(:admin => current_user.id), options_for_me(:flow => :deleted).merge(pagination_params))
-#    @second_nav = 'pages'
-#    handle_rss(
-#      :link => full_url,
-#      :title => 'Crabgrass Trash',
-#      :image => avatar_url(:id => @user.avatar_id||0, :size => 'huge')
-#    ) or render(:action => 'list')
-  end
-
-  def list_new
-    @field = 'updated'
-
-    @months = Page.month_counts(:current_user => (current_user if logged_in?), :field => @field, :event => true)
-    unless @months.empty?
-      @current_year = (Date.today).year
-      @start_year = @months[0]['year'] || @current_year.to_s
-      @current_month = (Date.today).month
-
-      # normalize path
-      unless @path.keyword?('date')
-        @path.merge!( :date => ("%s-%s" % [@months.last['year'], @months.last['month']]) )
-      end
-      @path.set_keyword(@field)
-
-      # find pages
-      @pages = Page.paginate_by_path( @path.merge(:type => 'EventPage'), options_for_me.merge(pagination_params))
-#      add_user_participations(@pages) if logged_in?
-    end
-#    @tags = Tag.for_group(:group => @group, :current_user => (current_user if logged_in?))
-#    search_template('archive')
-  end
 
   def list
     @month = (params[:month] || Time.zone.now.month).to_i
@@ -69,40 +28,9 @@ class Me::CalendarController < Me::BaseController
     #
     # @event_strips = Event.event_strips_for_month(@shown_month, :include => :some_relation, :conditions => 'some_relations.some_column = true')
     #
-   end
-
-  # post required
-  def update
-    pages = params[:pages]
-    if pages
-      pages.each do |page_id|
-        page = Page.find_by_id(page_id)
-        if page
-          if params[:undelete] and may_undelete_page?(page)
-            page.undelete
-          elsif params[:remove] and may_remove_page?(page)
-            page.destroy
-            ## add more actions here later
-          end
-        end
-      end
-    end
-    if params[:path]
-      #redirect_to :action => 'search', :path => params[:path]
-    else
-      #redirect_to :action => nil, :path => nil
-    end
   end
 
   protected
-
-  def may_undelete_page?(page)
-    current_user.may?(:admin, page)
-  end
-
-  def may_remove_page?(page)
-    current_user.may?(:delete, page)
-  end
 
   def context
     super

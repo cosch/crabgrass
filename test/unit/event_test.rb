9a4b1550df3247b77cf7e809c790e087b50518b0
cosch <github@torfbruecke.net>
2012-08-30 11:56:11 +0200

    unit test for events

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -1,10 +1,15 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  fixtures :users, :events, :profiles, :sites, :groups
+  fixtures :users, :events, :profiles, :sites, :groups, :pages, :memberships, :user_participations, :group_participations
   
   def setup
     enable_site_testing
+    @s = Date.civil( Time.zone.now.year.to_i, Time.zone.now.month.to_i).to_time
+    @e = Date.civil( Time.zone.now.year.to_i, Time.zone.now.month.to_i+1).to_time
+
+    @s_n = Date.civil( Time.zone.now.year.to_i, Time.zone.now.month.to_i+1).to_time
+    @e_n = Date.civil( Time.zone.now.year.to_i, Time.zone.now.month.to_i+2).to_time
   end
 
   def teardown
@@ -13,30 +18,85 @@ class EventTest < ActiveSupport::TestCase
 
 
   def test_finding_month_events
-    true
+    cond = { }
+    evts = Event.events_for_date_range(@s,@e,cond)
+
+    assert_equal 3, evts.count, "Should find 3 events"
+
+    evts.each do | e |  
+      assert_equal true, (e.starts_at > @s && e.ends_at < @e), "Event should be in this month"
+    end
   end
 
   def test_finding_next_month_events
-    true
+    cond = { }
+
+    evts = Event.events_for_date_range(@s_n,@e_n,cond)
+    assert_equal 2, evts.count, "Should find 2 events"
+    evts.each do |e| 
+	assert_equal true, (e.starts_at > @s_n && e.ends_at < @e_n), "Event should be in next month"
+    end
   end
 
   def test_finding_events_for_user_only
     user = users(:blue)
+    
+    cond = { :user => user }
+    evts = Event.events_for_date_range(@s,@e,cond)
+    assert_equal 1, evts.count, "Should find 1 events"
+    evts.each do |e|
+        assert_equal true, (e.starts_at > @s && e.ends_at < @e), "Event should be this month"        
+    end
   end
 
   def test_finding_events_with_unauth_user
     user = UnauthenticatedUser.new
-    true
+    cond = { :user => user }
+    evts = Event.events_for_date_range(@s,@e,cond)
+    assert_equal 0, evts.count, "Should find 0 events"
   end
 
   def test_finding_events_from_groups_for_user
     user = users(:blue)
-    true
+    cond = {:user=>user }
+
+    evts = Event.events_for_date_range(@s_n,@e_n,cond)
+    assert_equal 1, evts.count, "Should find 1 events for blue from rainbows group"
+    evts.each do |e|
+        assert_equal true, (e.starts_at > @s_n && e.ends_at < @e_n), "Event should be in next month"
+    end
   end
 
-  def test_finding_events_from_unauthed_group_suer
-    user = users(:red)
-    true
+  def test_finding_events_from_unauthed_group_user
+    user = users(:dolphin)
+    cond = {:user=>user }
+
+    evts = Event.events_for_date_range(@s_n,@e_n,cond)
+    assert_equal 0, evts.count, "Should find 0 events for dolphin from animal group"
+    evts.each do |e|
+        assert_equal true, (e.starts_at > @s_n && e.ends_at < @e_n), "Event should be in next month"
+    end
   end
 
+  def test_finding_events_from_group_for_group
+    group = groups(:rainbow)
+    cond = {:group=>group } 
+
+    evts = Event.events_for_date_range(@s_n,@e_n,cond)
+    assert_equal 1, evts.count, "Should find 1 events for rainbow group"
+    evts.each do |e|
+        assert_equal true, (e.starts_at > @s_n && e.ends_at < @e_n), "Event should be in next month"
+    end
+  end  
+
+  def test_finding_events_from_different_group_for_group
+    group = groups(:animals)
+    cond = {:group=>group }
+
+    evts = Event.events_for_date_range(@s_n,@e_n,cond)
+    assert_equal 0, evts.count, "Should find 0 events for animal group"
+    evts.each do |e|
+        assert_equal true, (e.starts_at > @s_n && e.ends_at < @e_n), "Event should be in next month"
+    end
+  end
 end

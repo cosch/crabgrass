444aee32c1b88fd81759a5077ba84d6760008aa4
cosch <github@torfbruecke.net>
2012-09-07 21:50:31 +0200

    found email in UID field on freebsd and some keys

--- lib/keyring.rb
+++ lib/keyring.rb
@@ -36,6 +36,10 @@ class Keyring
     if status
       fingerprint  = output.grep(/^fpr/).first.sub(/^.*:([A-F0-9]{40}):.*$/m, '\1') rescue Exception
       email        = output.grep(/^pub/).first.split(':')[9] rescue Exception
+      #on freebsd and with some keys it was in uid field
+      if email==""
+        email        = output.grep(/^uid/).first.split(':')[9] rescue Exception
+      end
     end
     {:fingerprint => fingerprint, :email => email}
   end
@@ -53,14 +57,43 @@ class Keyring
     File.unlink(output_path)
   end
 
+  def encrypt_to_new(fingerprint, data)
+    data_file = Tempfile.new('crabgrass_tmp')
+    data_file.write(data)
+    data_file.close
+
+    recipents = []
+    if fingerprint.is_a? String;
+      recipents.push '--recipient'
+      recipents.push fingerprint       
+    elsif fingerprint.is_a? Array 
+      fingerprint each do |f|
+        recipents.push '--recipient'
+        recipents.push fingerprint
+      end
+    end
+
+
+    output_path = fingerprint.is_a?(String) ? random_temp_filename(fingerprint) : random_temp_filename(fingerprint[0]) 
+    r = #{recipents.collect{|arg| arg.shell_escape}.join(' ')}
+    puts r
+    cmd('--encrypt', '--armor', r, '--trust-model', 'always', '--output', output_path, data_file.path)
+    File.read(output_path)
+  ensure
+    data_file.unlink
+    File.unlink(output_path) 
+  end
+
   def cmd(*args)
     cmdstr = "#{GPG_COMMAND} --no-default-keyring --keyring #{@path} " + args.collect{|arg| arg.shell_escape}.join(' ') + ' 2>&1'
     output = `#{cmdstr}`
+    puts cmdstr
+    puts output
     return [$?.success?, output]
   end
 
   def random_temp_filename(fingerprint)
-    "/tmp/#{rand 100000}{fingerprint}"
+    "/tmp/#{rand 100000}#{fingerprint}"
   end
 
 end

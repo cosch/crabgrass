103e57fed72bbc099c799dec4f6a6d8e0ffa58cd
cosch <github@torfbruecke.net>
2012-09-08 21:45:41 +0200

    bugreports encrypted as well

--- app/models/mailers/bugreport.rb
+++ app/models/mailers/bugreport.rb
@@ -10,6 +10,18 @@ module Mailers::Bugreport
       :error_action=>params[:error_action], :exception_message => params[:exception_detailed_message],
       :comments => params[:comments]})
     content_type "text/plain"
+
+    # we try to map the dev email to an account
+    Ã# or stop sending it
+    if Conf.gpg_emails_only
+      user = User.find_by_email(options[:dev_email])
+      if user
+        ensure_encryption_if_needed user, "request_to_join_us.erb"
+      else
+        raise Exception
+      end
+    end
+
   end
 
 end

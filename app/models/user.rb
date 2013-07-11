70cdf5b05edcab2990b1d7e6aae8729ed6f0a36b
cosch <cosch>
2013-07-11 20:50:50 +0000

    online means 3 mins last seen now

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,7 +72,7 @@ class User < ActiveRecord::Base
     opts = {
       :order => 'login ASC'
     }    
-    opts[:conditions] = ['last_seen_at > ?', 10.minutes.ago]
+    opts[:conditions] = ['last_seen_at > ?', 3.minutes.ago]
 
     opts
   }

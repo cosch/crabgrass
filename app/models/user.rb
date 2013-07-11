65164908f128079278db706edf6dd667beabf67d
cosch <github@torfbruecke.net>
2013-07-11 23:06:23 +0200

    online -> 3min

--- app/models/user.rb
+++ app/models/user.rb
@@ -149,7 +149,7 @@ class User < ActiveRecord::Base
   end
 
   def online?
-    last_seen_at > 10.minutes.ago if last_seen_at
+    last_seen_at > 3.minutes.ago if last_seen_at
   end
 
   def time_zone
